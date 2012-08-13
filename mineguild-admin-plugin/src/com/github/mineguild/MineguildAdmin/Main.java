eee4ab4bbc88ba8574c34227ff60107ab42c5713
nylser <korbinian.stein@gmail.com>
2012-08-13 21:35:11 +0200

    Changed the command executor to CMDEXE.

--- mineguild-admin-plugin/src/com/github/mineguild/MineguildAdmin/Main.java
+++ mineguild-admin-plugin/src/com/github/mineguild/MineguildAdmin/Main.java
@@ -3,14 +3,14 @@ package com.github.mineguild.MineguildAdmin;
 
 public class Main extends org.bukkit.plugin.java.JavaPlugin {
 //Calling MGACommandExecutor as Executor
-private MGACommandExecutor myExecutor;
+private MGACommandExecutor CMDEXE;
 @Override
 //Enabling Plugin
 public void onEnable(){
-myExecutor = new MGACommandExecutor (null);
+CMDEXE = new MGACommandExecutor (null);
 //Sending commands to MGACommandExecutor
-getCommand("mga").setExecutor(myExecutor);
-getCommand("gm").setExecutor(myExecutor);
+getCommand("mga").setExecutor(CMDEXE);
+getCommand("gm").setExecutor(CMDEXE);
 //Send message to console
 getLogger().info("MineguildAdmin has been enabled!");
 }
