3994b6b8c45dd6ed4582d80eb07ba2810028bd34
nylser <korbinian.stein@gmail.com>
2012-08-13 14:37:01 +0200

    Adding PlayerListener (Testing)

---
+++ mineguild-admin-plugin/src/com/github/mineguild/MineguildAdmin/PlayerListener.java
@@ -0,0 +1,13 @@
+package com.github.mineguild.MineguildAdmin;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
+
+public class PlayerListener implements Listener {
+	@EventHandler
+	public void onPlayerLoginEvent(PlayerLoginEvent event){
+		
+	}
+
+}
