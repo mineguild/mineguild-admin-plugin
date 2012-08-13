2ac3f09f35519526e9ff619b064a6854786cf619
nylser <korbinian.stein@gmail.com>
2012-08-13 14:23:51 +0200

    Adding disable & enable Messages.

--- mineguild-admin-plugin/src/com/github/mineguild/MineguildAdmin/Main.java
+++ mineguild-admin-plugin/src/com/github/mineguild/MineguildAdmin/Main.java
@@ -3,11 +3,11 @@ package com.github.mineguild.MineguildAdmin;
 public class Main extends org.bukkit.plugin.java.JavaPlugin {
 //Enabling Plugin
 public void onEnable(){
-	
+	getLogger().info("MineguildAdmin has been enabled!");
 }
 //Disabling Plugin
 public void onDisable(){
-	
+	getLogger().info("MineguildAdmin has been disabled!");
 }
 
 }
