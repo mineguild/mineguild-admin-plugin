a2f99bb4c70c04e890364606c3022ad338f2719e
nylser <korbinian.stein@gmail.com>
2012-08-13 14:39:45 +0200

    Commenting code.

--- mineguild-admin-plugin/src/com/github/mineguild/MineguildAdmin/PlayerListener.java
+++ mineguild-admin-plugin/src/com/github/mineguild/MineguildAdmin/PlayerListener.java
@@ -1,9 +1,9 @@
 package com.github.mineguild.MineguildAdmin;
-
+//Import the needed methods
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
-
+//PlayerListener, dont know what im doing there...
 public class PlayerListener implements Listener {
 	@EventHandler
 	public void onPlayerLoginEvent(PlayerLoginEvent event){
