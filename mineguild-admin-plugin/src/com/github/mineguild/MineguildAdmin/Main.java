0d52706d7b23a561061b42ceca7af4e546bb67a1
nylser <korbinian.stein@gmail.com>
2012-08-13 14:55:07 +0200

    Some Changes Adding a test command. Cleaning up PlayerListener

--- mineguild-admin-plugin/src/com/github/mineguild/MineguildAdmin/Main.java
+++ mineguild-admin-plugin/src/com/github/mineguild/MineguildAdmin/Main.java
@@ -1,5 +1,8 @@
 package com.github.mineguild.MineguildAdmin;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
 public class Main extends org.bukkit.plugin.java.JavaPlugin {
 //Enabling Plugin
 public void onEnable(){
@@ -8,5 +11,13 @@ public void onEnable(){
 //Disabling Plugin
 public void onDisable(){
 	getLogger().info("MineguildAdmin has been disabled!");
+}
+public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+	if(cmd.getName().equalsIgnoreCase("test")){
+		//Do nothing only return true
+		return true;
+	}
+	//Else return false
+	return false;
 }
 }
--- mineguild-admin-plugin/src/com/github/mineguild/MineguildAdmin/PlayerListener.java
+++ mineguild-admin-plugin/src/com/github/mineguild/MineguildAdmin/PlayerListener.java
@@ -1,11 +1,14 @@
 package com.github.mineguild.MineguildAdmin;
+
 //Import the needed methods
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
+
 //PlayerListener, dont know what im doing there...
 public class PlayerListener implements Listener {
-	@EventHandler
+	@EventHandler(priority = EventPriority.LOW) //Setting priority to low we don´t need that now
 	public void onPlayerLoginEvent(PlayerLoginEvent event){
 		
 	}
