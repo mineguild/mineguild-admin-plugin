2853c3691efe98b5ab5af4d74b63c6c4362b8f23
nylser <korbinian.stein@gmail.com>
2012-08-13 17:35:09 +0200

    Added CommandExecutor for /version command.

--- mineguild-admin-plugin/src/com/github/mineguild/MineguildAdmin/Main.java
+++ mineguild-admin-plugin/src/com/github/mineguild/MineguildAdmin/Main.java
@@ -2,16 +2,19 @@ package com.github.mineguild.MineguildAdmin;
 
 
 public class Main extends org.bukkit.plugin.java.JavaPlugin {
+//Calling MGACommandExecutor as Executor
 private MGACommandExecutor myExecutor;
 @Override
 //Enabling Plugin
 public void onEnable(){
 myExecutor = new MGACommandExecutor ();
+//Sending commands to MGACommandExecutor
 getCommand("test").setExecutor(myExecutor);
+getCommand("version").setExecutor(myExecutor);
 getLogger().info("MineguildAdmin has been enabled!");
 }
 //Disabling Plugin
 public void onDisable(){
-	getLogger().info("MineguildAdmin has been disable!");
+	getLogger().info("MineguildAdmin has been disabled!");
 }
 }
